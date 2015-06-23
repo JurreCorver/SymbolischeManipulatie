ce4e34f5b5eb793ac305b82fc16ea9da1fa77b4c
janwillemvanittersum <janwillemvanittersum@gmail.com>
2015-06-23 09:54:26 +0200

    Problems met leading coefficient

--- numtheory.py
+++ numtheory.py
@@ -1,9 +1,13 @@
 from expression_template import *
 
 def leadingCoefficient(exp,var='x'):
+    #simplify exp to the form a_n x^n + \ldots + a_0
+    exp=simplify(exp)
+    #if the expression consist of one term (e.g. 5x**7)
+#    if isinstance(exp,)
+
     #split the expressions in its terms and search for the term with the degree of the expression
-    #this works, because simplify assures that exp is of the form a_n x^n + \ldots + a_0
-    terms=getCommList(simplify(exp))
+    terms=getCommList(exp)
     for term in terms:
         if term.deg(var)==exp.deg(var):
             #the leadingCoefficient of a constant is the constant, lhs of x**7 is x, while lhs of 5*x**7 is 5
@@ -15,9 +19,13 @@ def leadingCoefficient(exp,var='x'):
                 return term.lhs
 
 def polDivMod(exp1,exp2, var='x'):
+    #modulo 0 nothing happens
+    if exp2 == Constant(0):
+        return [Constant(0),exp1]
+
     totdiv = Constant(0)
     while exp1.deg(var)>= exp2.deg(var):
-        div = leadingCoefficient(exp1,var) * Variable(var)**(Constant(exp1.deg(var) - exp2.deg(var)))
+        div = leadingCoefficient(exp1,var)/leadingCoefficient(exp2,var) * Variable(var)**(Constant(exp1.deg(var) - exp2.deg(var)))
         exp1 = simplify(exp1 - div * exp2)
         totdiv = totdiv + div
     return [simplify(totdiv), exp1]
