4fed525df97f033cbfebfd4aea971f2cab53daba
janwillemvanittersum <janwillemvanittersum@gmail.com>
2015-06-25 11:42:10 +0200

    execute

--- execute_JW.py
+++ execute_JW.py
@@ -8,13 +8,6 @@ exp2=sfrost('(x+1)*(x-2)')
 exp1=sfrost('(x+1)*(x-3)')
 var='x'
 
-print(exp1,',', exp2)
-
-totquot = Constant(0)
-
-deg1 = exp1.deg(var)
-deg2 = exp2.deg(var)
-quot = coefficient(exp1,deg1, var)/coefficient(exp2,deg2,var) * Variable(var)**(Constant(exp1.deg(var) - exp2.deg(var)))
-exp1 = simplify(exp1 - quot * exp2)
-totquot = totquot + quot
-print(simplify(frost('-3 - 2 * x - (-2 - x + x ** 2) + x ** 2')))
+print(subtoadd(frost('5-(-2 - x + x ** 2)')))
+print(type(frost('(-2 - x + x ** 2)')) == AddNode and type('1')!=AddNode)
+print(expand(frost('-1')))
