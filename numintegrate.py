05369d09be81bebc5d0d6c616786f1e543da81c3
janwillemvanittersum <janwillemvanittersum@gmail.com>
2015-07-03 08:46:29 +0200

    informatieve comments

--- numintegrate.py
+++ numintegrate.py
@@ -3,14 +3,18 @@ import numpy as np
 
 def numInt(expression, var, left, right, numsteps=10000):
     ans=0
+
+    #convert the constants in the input to numbers
     left=float(left)
     right=float(right)
     numsteps=float(numsteps)
-    stepsize=abs(right-left)/numsteps
-    points = np.arange(left, right, stepsize)
+    stepsize=abs(right-left)/(numsteps-1) #-1 because right should always be the last point
+
+    #create a set of points where we evaluate the expression
+    points =  np.append(np.arange(left, right, stepsize),right)
     l=len(points)
     for point in points:
-        ans+= num(expression.evaluate({var.symbol:Constant(point)}))/l
+        ans+= num(expression.evaluate({var.symbol:Constant(point)}))/l #neem het gemiddelde over alle punten waar de
     return Constant(ans)
 
 methodList.append(['numIntegrate',numInt,5])
